a3b9f927ba7c86e7548acfd33207462d6dbdda3e
José Miguel <a19esrujo@iesgrancapitan.org>
2022-12-08 11:32:44 +0100

    add:calculator v2.0.0 comportamiento básico

--- README.md
+++ README.md
@@ -1,15 +1,41 @@
-# Tarea 3.9. Creando elementos del DOM: Calculadora
+# Tarea 4.1. Calculadora mediante objeto literal
 
 ## **Alumno: José Miguel Escribano Ruiz 2ºDAW-A**
 
-Partiendo de un documento html vacío, crea los elementos HTML de una calculadora mediante los métodos del objeto predefinido document. Ni document.write() ni fichero.css están permitidos. Entrégala en este repositorio.
+En este repositorio entrega la calculadora de la tarea anterior. Toma como base [esta calculadora](https://www.webestools.com/scripts_tutorials-code-source-8-javascript-calculator-buttons-calculator-keyboard-support-operations-modulo.html). Recuerda:
 
-Sed eficientes y evitad la duplicación de código.
+- Parte de un documento html vacío.
+  
+- Crea los elementos HTML de la calculadora mediante los métodos del objeto predefinido document. Ni tablas ni li, ni document.write() ni fichero.css están permitidos. Dale un buen uso a las etiquetas.
+  
+Añádele ahora el comportamiento del display bien controlado (ni +, ni -, ni x ni %):
 
-![calculadora](img/calculadora.png)
+1. Inicialmente en el display aparece el cero sin decimal.
+2. En el display sólo puede aparecer un punto decimal.
+3. A la izquierda del punto sólo puede aparecer un cero ("00.1" no es válido).
+4. No hay que escribir "0." para que te acepte el decimal. Basta con que pulse la coma decimal. Entonces el resto se consideran decimales.
+5. En el display siempre ha de haber un dígito. En caso de usar el retroceso y ser el último carácter aparecerá un cero.
+6. El cero negativo no existe ("-0" no es válido).
+   
+El diseño parte del ejercicio del tema anterior. Procura que el patrón de diseño sea con un objeto literal.
 
-## Enlaces al código
+Ten en cuenta los siguientes detalles:
+
+- Usa funciones arrow en la medida de lo posible.
+- Evita el uso del for clásico
+- No uses document.get... y añade el comportamiento conforme los vayas creando
+
+## Referencia 
+
+- [Learning JavaScript Design Patterns](https://www.patterns.dev/posts/classic-design-patterns/#modulepatternjavascript) (Object literals)
+- Module Pattern - ES5/Classic Approach
+- [syntax introduced in ES5](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Method_definitions#description)
+
+## link al código
 
 - [index.html](index.html)
-  
+
 - [script.js](script.js)
+
+
+
--- script.js
+++ script.js
@@ -3,49 +3,118 @@
  * Ni document.write() ni fichero.css están permitidos.
  * 
  * @author josemi
+ * @version 2.0.0 Objeto Predefinido Calculadora
+ * 
  */
 
-document.addEventListener('DOMContentLoaded',function(){
-    
-    const arraySimbolos = ['CE','←','%','+','7','8','9','-','4','5','6','x','1','2','3','÷','0','±',',','='];
+{
 
-    // crear h1 y aplicar css
-    let h1 = document.createElement('h1');
-    h1.innerHTML = 'Calculadora';
-    document.body.appendChild(h1);
-    h1.style.textAlign = 'center';
-    
-    // crear div
-    let div = document.createElement('div');
-    document.body.appendChild(div);
-    
-    // css al div mediante javascript
-    div.style.display = 'grid';
-    div.style.gridTemplateColumns = 'repeat(4,1fr)';
-    div.style.width = '300px';
-    div.style.height = '300px';
-    div.style.margin = '0 auto';
-    div.style.border = '1px solid black';
-
-    // crear input y añadir css mediante javascript
+    //variables globales
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
     let input = document.createElement('input');
-    input.value = 0;
-    input.setAttribute('readonly',''); 
 
-    input.style.textAlign = 'right';
-    input.style.gridColumn = "1 / 5";
-    input.style.margin = "4px";
-    input.style.padding = "4px";
-    div.appendChild(input);
-    
+    const calculadora = {
 
-    // mostrar botones
-    arraySimbolos.forEach(element => {
-        let boton = document.createElement('button');
-        boton.innerHTML = element;
-        div.appendChild(boton);
-        boton.style.margin = '4px';
-        boton.style.padding= '4px';
-    });
+        init: () => {
+            calculadora.crearDiv(),
+                calculadora.crearInput(),
+                calculadora.crearBotones();
+        },
 
-});
+        comportamiento(valor) {
+
+            if (valor <= '9' && valor >= '1') {
+                return () => {
+                    input.value += valor; //concatenamos el nuevo valor podemos usar input.innerhtml
+                }
+            }
+
+            switch (valor) {
+
+                case 'CE':
+                    return () => {
+                        input.value = 0;
+                    }
+
+                case '←':
+
+                    return () => {
+                        input.value = input.value.substring(0, (input.value.length - 1));
+                        // vamos borrando el último elemento 
+                        if (input.value == "") {
+                            input.value = 0;
+                        }
+                    }
+
+                case ',':
+
+                    return () => {
+                        if (!input.value.includes('.')) {
+                            //si el imput no contiene un punto, se lo añadimos cuando lo pulsamos
+                            input.value += '.';
+                        }
+                    }
+
+                case '0':
+
+                    return () => {
+                        if (input.value.startsWith('0')) {
+                            input.value = valor;
+                        } 
+                        if (input.value.includes('.')) {
+                            input.value += valor;
+                        }
+                    }
+                default:
+                    break; //sino puede hacer nada rompe el ciclo
+            }
+        },
+
+        crearDiv: () => {
+
+            //h1 y su css
+            h1.innerHTML = 'Calculadora';
+            h1.style.textAlign = 'center';
+            document.body.appendChild(h1);
+
+            // css al div mediante javascript
+            div.style.display = 'grid';
+            div.style.gridTemplateColumns = 'repeat(4,1fr)';
+            div.style.width = '400px';
+            div.style.height = '400px';
+            div.style.margin = '0 auto';
+            div.style.border = '1px solid black';
+            document.body.appendChild(div);
+        },
+
+        crearInput: () => {
+            input.value = 0;
+            input.setAttribute('readonly', '');
+
+            input.style.textAlign = 'right';
+            input.style.fontSize = '30px';
+            input.style.gridColumn = "1 / 5";
+            input.style.margin = "4px";
+            input.style.padding = "4px";
+            div.appendChild(input);
+        },
+
+        crearBotones: () => {
+            const arraySimbolos = ['CE', '←', '%', '+', '7', '8', '9', '-', '4', '5', '6', 'x', '1', '2', '3', '÷', '0', '±', ',', '='];
+            arraySimbolos.forEach(element => {
+                let boton = document.createElement('button');
+                boton.innerHTML = element;
+                boton.style.margin = '4px';
+                boton.style.padding = '4px';
+                boton.style.fontSize = '30px';
+                div.appendChild(boton);
+                // le pasamos el comportamiento cada vez que se hace click en un botón
+                boton.addEventListener('click', calculadora.comportamiento(element));
+            });
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", calculadora.init);
+
+}
